3a3584f5b290660d05d8240e6a78d8c47e49dd75
Admin <vanpho02@gmail.com>
2017-02-20 19:48:18 +0700

    sign

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// https://github.com/vanpho93/jsonwebtoken2311/blob/master/jwt.pdf
+let jwt = require('jsonwebtoken');
+
+let obj = {name: 'Pho', age: 18};
+const SECRET_KEY = '7e32nsjdg379de3efi';
+
+let token = jwt.sign(obj, SECRET_KEY, {expiresIn: 10});
+console.log(token);
