c6d20342d3c8b4920b54f0b40f449a0998877db9
Admin <vanpho02@gmail.com>
2017-02-20 19:57:08 +0700

    verify

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ let obj = {name: 'Pho', age: 18};
 const SECRET_KEY = '7e32nsjdg379de3efi';
 
 let token = jwt.sign(obj, SECRET_KEY, {expiresIn: 10});
-console.log(token);
+// console.log(token);
+
+jwt.verify(token, SECRET_KEY, (err, decoded) => {
+  if (err) return console.log(err);
+  console.log(decoded);
+});
