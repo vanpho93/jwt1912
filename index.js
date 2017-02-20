52a019e832904cbf878b9c6ae3b458ad4caf671d
Admin <vanpho02@gmail.com>
2017-02-20 20:03:38 +0700

    wrap to function

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ let jwt = require('jsonwebtoken');
 let obj = {name: 'Pho', age: 18};
 const SECRET_KEY = '7e32nsjdg379de3efi';
 
-let token = jwt.sign(obj, SECRET_KEY, {expiresIn: 10});
-// console.log(token);
-
-jwt.verify(token, SECRET_KEY, (err, decoded) => {
+jwt.verify(token, '3455', (err, decoded) => {
   if (err) return console.log(err);
   console.log(decoded);
 });
+
+let getToken = (obj) => jwt.sign(obj, SECRET_KEY, {expiresIn: 3});
