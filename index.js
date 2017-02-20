3275df5c61eb63016d277668545dc15d3b44bb55
Admin <vanpho02@gmail.com>
2017-02-20 20:14:29 +0700

    export

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ let jwt = require('jsonwebtoken');
 let obj = {name: 'Pho', age: 18};
 const SECRET_KEY = '7e32nsjdg379de3efi';
 
-jwt.verify(token, '3455', (err, decoded) => {
-  if (err) return console.log(err);
-  console.log(decoded);
-});
+let getObject = (token, cb) => {
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) return cb(err);
+    return cb(undefined, decoded);
+  });
+}
 
-let getToken = (obj) => jwt.sign(obj, SECRET_KEY, {expiresIn: 3});
+let getToken = obj => jwt.sign(obj, SECRET_KEY, {expiresIn: 3});
+
+let token1 = getToken(obj);
+
+getObject(token1, (err, decoded) => {
+  if(err) return console.log(err + '');
+  console.log(decoded);
+})
+
+module.exports = {getToken, getObject};
